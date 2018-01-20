a64680c6bcb3c1ce6d7b9c74a19d9ec9c893205c
Andrew Mfune <andiemfune@gmail.com>
2018-01-21 09:22:08 +1200

    Initializing global properties
    The puzzle setup has been updated to initialise global properties such as totalBlocks and matchingBlocks with default null values.

--- scripts/puzzle_setup.py
+++ scripts/puzzle_setup.py
@@ -1,5 +1,6 @@
 from puzzle import PuzzleLoader
 from bge import logic
+from objproperties import ObjProperties
 
 DEF_NUM_STRUCT =  {
     1 : [1, 2, 3, 4], 
@@ -9,9 +10,20 @@ DEF_NUM_STRUCT =  {
 }
 
 def main(controller):
+    own = ObjProperties(controller.owner)
     scene = logic.getCurrentScene()
     puzzle = PuzzleLoader(scene)
+    initializeProperties(puzzle)
+    
     puzzle.setStaticBlockNumbers(DEF_NUM_STRUCT)
     puzzle.addLogicalBlocks()
     puzzle.setLogicalBlockNumbers()
     puzzle.addVisualBlocks()
+    
+
+def initializeProperties(puzzle):
+    globDict = logic.globalDict
+    controller = logic.getCurrentController()
+    own = ObjProperties(controller.owner)
+    globDict['matchingBlocks'] = {}
+    globDict['totalBlocks'] = len(puzzle.getStaticBlocks()) -1
