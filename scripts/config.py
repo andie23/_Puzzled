8b4d6f568beacd300cea6f560be40f8557371fa1
unknown <Andrew Mfune>
2018-05-16 21:34:32 +0100

    Directory definitions moved to config.py
    The config file has been updated with constants pointing to the game's
    data directory, a directory that will hold log statements and database
    files. A function has also been created that checks if the data directory
    exists in the HOMEPATH and creates one if it doesnt.
    
    In the logger module, directory management called has been deleted and is
    loaded instead from the config file.;

--- scripts/config.py
+++ scripts/config.py
@@ -1,8 +1,38 @@
 from bge import events
+from os import environ, mkdir
 
+HOME_PATH = environ['HOMEPATH']
+'''
+Databases and log file will be kept in DATA_DIR
+'''
+DATA_DIR  = 'puzzled'
+LOG_FILE_NAME = 'puzzled.log'
+
+'''
+Control button configuration
+'''
 BUTTON_CONFIG = {
     'UP': events.UPARROWKEY,
     'DOWN' : events.DOWNARROWKEY,
     'LEFT' : events.LEFTARROWKEY,
     'RIGHT': events.RIGHTARROWKEY
 }
+
+
+'''
+Return full path of the game's directory. The game directory is 
+created if not found.
+'''
+def dataDirPath():
+    path = '{0}\{1}'.format(HOME_PATH, DATA_DIR)
+        
+    if DATA_DIR not in HOME_PATH:
+        mkdir(path)
+            
+    return path
+
+'''
+Returns full path of where the log file is located
+'''
+def logPath():
+    return '{0}\{1}'.format(dataDirPath(), LOG_FILE_NAME)
--- scripts/logger.py
+++ scripts/logger.py
@@ -1,8 +1,31 @@
 import logging
+from os import environ, mkdir
 
-def logger():
+LOG_FILE_PATH = environ['HOMEPATH']
+LOG_DIR_NAME  = 'puzzled'
+LOG_FILE_NAME = 'puzzled.log'
+
+'''
+Returns logger object
+'''
+def logger():   
+    path = logPath()
     logging.basicConfig(
-        filename='/logs/puzzled.log', level=logging.DEBUG,
+        filename=path, level=logging.DEBUG,
         format='%(asctime)s: %(levelname)s: %(message)s'
     )
     return logging
+
+'''
+Return path of log file. Creates log directory if none exists
+'''
+def logPath():
+    path = '{0}\{1}'.format(LOG_FILE_PATH, LOG_DIR_NAME)
+    fullPath = '{0}\{1}'.format(path, LOG_FILE_NAME)
+    
+    if LOG_DIR_NAME not in LOG_FILE_PATH:
+        mkdir(path)
+            
+    return fullPath
+    
+    
