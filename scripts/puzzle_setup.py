d4c8178bd8ca67e4e9dbc4d98456fbee5261f060
Andrew Mfune <andiemfune@gmail.com>
2018-06-01 09:43:29 +0800

    Patterns have names instead of numeric indexes
    Puzzle patterns are now stored in a dictionary instead of list. Each
    pattern is now indexed with a unique name

--- scripts/puzzle_setup.py
+++ scripts/puzzle_setup.py
@@ -10,14 +10,14 @@ from bge import logic
 from objproperties import ObjProperties
 from logger import logger
 from patterns import PUZZLE_PATTERNS_4X4
-from state_scripts import DEFAULT
+from state_scripts import DEFAULT, ALERT_MODE
 
 def main(controller):
     own = ObjProperties(controller.owner)
     scene = logic.getCurrentScene()
     puzzle = PuzzleLoader(scene)
     initializeProperties(puzzle)
-    puzzle.setStaticBlockNumbers(PUZZLE_PATTERNS_4X4[0])
+    puzzle.setStaticBlockNumbers(PUZZLE_PATTERNS_4X4['CLASSIC'])
     puzzle.addLogicalBlocks()
     puzzle.setLogicalBlockNumbers()
     puzzle.addVisualBlocks()
@@ -31,5 +31,5 @@ def initializeProperties(puzzle):
     own = ObjProperties(controller.owner)
     globDict['matchingBlocks'] = {}
     globDict['totalBlocks'] = len(puzzle.getStaticBlocks()) -1
-    globDict['blockEventStatesDef'] = DEFAULT
+    globDict['blockEventStatesDef'] = ALERT_MODE
     
