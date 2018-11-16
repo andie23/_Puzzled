114531d1490dc65421ca5e2d02f7888597da04a4
Andrew Mfune <andiemfune@gmail.com>
2018-11-16 07:26:49 -0800

    Refactored animation module by adding docstrings in some methods and reducing code size

--- scripts/animate.py
+++ scripts/animate.py
@@ -2,56 +2,73 @@ from bge import logic
 from navigator import SceneHelper
 from objproperties import ObjProperties
 
-def check_target_object(func):
-    def main(*args, **kwargs):
-        shelper = SceneHelper(logic)
-        cont = logic.getCurrentController()
-        own = ObjProperties(cont.owner)
-        instanceID = own.getProp('instance_id')
-        
-        if instanceID in logic.globalDict:   
-            instanceData = logic.globalDict[instanceID]
-            scene = shelper.getscene(instanceData['scene_id'])
-            scenObjs = scene.objects
 
-            targetObj = own.getObjByPropVal('anim_id', instanceID, scenObjs)
+def check_target_object(func):
+    '''
+    Decorator for checking whether the target object being
+    animated is still in the scene. If its not, the animation instance 
+    is deleted.
+
+    '''
+
+    def main(*args, **kwargs):
+        own = logic.getCurrentController().owner
+        animId = own['instance_id']
+
+        if animId in logic.globalDict:   
+            animData = logic.globalDict[animId]
+            scene = SceneHelper(logic).getscene(
+                animData['scene_id']
+            )
+            targetObj = ObjProperties().getObjByPropVal(
+                'anim_id', animId, scene.objects
+            )
             if not targetObj:
-               return cont.owner.endObject()
+               return own.endObject()
         return func(*args, **kwargs) 
     return main
            
-def initAnimation(sceneID, data):
-    targetObj = data['target_obj']
-    instanceID = getObjID(targetObj)
+def initAnimation(sceneId, animData):
+    '''
+    Animation entry point. This must be called in other modules to
+    start animation process on any object.
+    '''
 
-    if instanceID not in logic.globalDict:
-        data['target_obj'] = setObjAnimID(targetObj, instanceID)
-        data['scene_id'] = sceneID
-        logic.globalDict[instanceID] = data
-        addAnimInstanceObj(instanceID, sceneID)   
+    animId = getAnimId(animData)
 
-def getObjID(obj):
-    return '_%s' % hash(obj)
+    if animId not in logic.globalDict:
+        animData['target_obj']['anim_id'] = animId
+        animData['scene_id'] = sceneId
+        logic.globalDict[animId] = animData
+        addAnimInstanceObj(animId, sceneId)   
 
-def setObjAnimID(obj, animID):
-    if 'anim_id' not in obj:
-        obj['anim_id'] = animID
-    return obj
+def getAnimId(dat):
+    return 'anim_%s_%s' % (
+        dat['anim_name'], hash(dat['target_obj'])
+    )
 
-def addAnimInstanceObj(instanceID, sceneID):
-    obj = ObjProperties()
-    shelper = SceneHelper(logic)
-    scene = shelper.getscene(sceneID)
-    idleInstance = obj.getPropObjGroup('anim_instance',  scene, 0)
+def addAnimInstanceObj(animId, sceneId):
+    '''
+    Adds an object in the scene that will  animate and manage 
+    animation for set target object.
+    '''
+
+    scene = SceneHelper(logic).getscene(sceneId)
+    idleInstance = ObjProperties().getPropObjGroup(
+        'anim_instance',  scene, 0
+    )
     idleInstance = idleInstance[0]
-    idleInstance['instance_id'] = instanceID   
+    idleInstance['instance_id'] = animId   
     scene.addObject(idleInstance)
 
 @check_target_object
 def run():
-    controller = logic.getCurrentController()
-    own = controller.owner['instance_id']
-    animData = logic.globalDict[own]
+    '''
+    Plays the animation only once.
+    '''
+
+    animId = logic.getCurrentController().owner['instance_id']
+    animData = logic.globalDict[animId]
     playOnce(
         animData['target_obj'],
         animData['anim_name'],
@@ -62,46 +79,48 @@ def run():
 
 @check_target_object
 def recordFrames():
-    own = logic.getCurrentController().owner
-    instanceID = own['instance_id']
-    instanceData = logic.globalDict[instanceID]
-    targetObj = instanceData['target_obj']
+    '''
+    Record current animation frames
+    '''
 
+    own = logic.getCurrentController().owner
+    animId = own['instance_id']
+    targetObj = logic.globalDict[animId]['target_obj']
     own['cur_frame'] = targetObj.getActionFrame()
 
 @check_target_object
 def onStart():
-    controller = logic.getCurrentController()
-    own = controller.owner
-    instanceID = own['instance_id']
-    instanceData = logic.globalDict[instanceID]
+    '''
+    Executes any method set in on_start_action when the animation
+    starts on frame 1.
+    '''
+
+    own = logic.getCurrentController().owner
+    animId = own['instance_id']
+    animData = logic.globalDict[animId]
     
-    if 'on_start_action' not in instanceData:
-        return
-    
-    fstart = instanceData['fstart']
-    curFrame = own['cur_frame']
-        
-    if curFrame >= 1.0:
-        own['is_start'] = True
-        instanceData['on_start_action']()
+    if 'on_start_action' in animData:
+        if own['cur_frame'] >= 1.0:
+            own['is_start'] = True
+            animData['on_start_action']()
 
 @check_target_object
 def onFinish():
-    controller = logic.getCurrentController()
-    own = controller.owner
-    instanceID = own['instance_id']
-    instanceData = logic.globalDict[instanceID]
-    
-    fstop = instanceData['fstop']
-    curFrame = own['cur_frame']
-                
-    if curFrame >= fstop:
-        if 'on_finish_action' in instanceData:
+    '''
+    Excutes callback after an animation finishes playing.
+    Animation instance is terminated here
+    '''
+ 
+    own = logic.getCurrentController().owner
+    animId = own['instance_id']
+    animData = logic.globalDict[animId]
+
+    if own['cur_frame'] >= animData['fstop']:
+        if 'on_finish_action' in animData:
             own['is_stop'] = True
-            instanceData['on_finish_action']()
+            animData['on_finish_action']()
         own.endObject()
-        del logic.globalDict[instanceID]
+        del logic.globalDict[animId]
         
 @check_target_object
 def playOnce(obj, name, fstart=0.0, fend=20.0, speed=1.0):
