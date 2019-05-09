6a5201116d5c7cb072adb423e0d5d05250f48eba
Andrew Mfune <andiemfune@gmail.com>
2019-05-08 20:11:30 -0800

    Using listerners in score module to increment moves, add or remove blocks from matchlist and building streaks

--- scripts/score.py
+++ scripts/score.py
@@ -5,22 +5,47 @@ from copy import deepcopy
 from navigator import overlayAssessment, SceneHelper
 from notification import showNotification
 from block import LogicalBlock
+from block_listerners import OnBlockMovementStartListerner, OnMatchBlockListerner, OnMisMatchBlockListerner
 from game import *
 import canvas
 
-def updateMatchList(controller):
+def init(controller):
     scene = logic.getCurrentScene()
     block = LogicalBlock(scene, controller.owner)
     matchList = getPuzzleState('match_list')
+    blockId = block.blockID
 
-    if block.isMatch:
-        buildstreak(block.blockID)
-        if block.blockID not in matchList:
-            addMatch(block.blockID)
-    else:
-        resetstreak()
-        if block.blockID in matchList:
-            removeMatch(block.blockID)
+    OnBlockMovementStartListerner(block).attach(
+        'increment_moves', incrementMoves
+    )
+
+    OnMatchBlockListerner(block).attach(
+        'add_to_matchlist', lambda: addBlockToMatchList(blockId, matchList)
+    )
+
+    OnMisMatchBlockListerner(block).attach(
+        'remove_from_matchlist', lambda: removeBlockFromMatchList(blockId, matchList)
+    )
+    
+    OnMatchBlockListerner(block).attach(
+        'build_streak', lambda: buildstreak(blockId)
+    )
+
+    OnMisMatchBlockListerner(block).attach(
+        'reset_match_streak', lambda: resetstreak()
+    )
+
+def incrementMoves():
+    moves = getPlayStats('moves') + 1 
+    setPlayStats('moves', moves)
+
+def addBlockToMatchList(blockId, matchList):
+    if blockId not in matchList:
+        addMatch(blockId)
+
+def removeBlockFromMatchList(blockId, matchList):
+    if blockId in matchList:
+        removeMatch(blockId)
 
 def checkSequence():
     if not isSessionSet():
