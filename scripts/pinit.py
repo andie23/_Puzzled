7a5fe0dee69139cb5130b450b0ef8d22e32c4c8a
Andrew Mfune <andiemfune@gmail.com>
2019-05-10 04:50:06 -0800

    Added game event listerners. Some functionalities originaly appearing in other modules have been shifted here

--- scripts/pinit.py
+++ scripts/pinit.py
@@ -1,22 +1,59 @@
-#########################################################
-# Author: Andrew Mfune
-# Date: 22/05/2018
-# Description: This module is an entry point to the puzzle
-#               game. States can be specified here and 
-#               the solve pattern too..
-#########################################################
-from navigator import overlayHud
-from loader import add_loading_screen
-from puzzle import PuzzleLoader
 from bge import logic
-from game import getPuzzleState, createSession
+from game_event_listerners import *
+from game import stop
+from block import SpaceBlock
+from notification import showNotification
+from navigator import *
+from global_dictionary import *
+from player import getPlayer
 
-@add_loading_screen
-def init():
-    overlayHud()
-    createSession()
+def init(controller):
+    overlayLoadingScreen()
     initPuzzleBoard(getPuzzleState('block_pattern'))
-    return True    
+    overlayHud()
+
+    OnGameStartListerner().attach(
+        'unlock_space_block', SpaceBlock(scene).unLock
+    )
+    
+    OnGameStartListerner().attach(
+        'remove_loading_screen', closeLoadingScreen
+    )
+
+    OnGameStopListerner().attach(
+        'lock_space_block', SpaceBlock(scene).lock
+    )
+
+    OnPuzzleReshuffleListerner().attach(
+        'restart_scene', SceneHelper(logic).restart(['MAIN', 'HUD']
+    )
+    
+    OnPuzzleReshuffleListerner().attach(
+        'add_loading_screen', overlayLoadingScreen
+    )
+
+    OnPuzzleCompleteListerner().attach(
+        'stop_game', stop
+    )
+
+    OnPuzzleCompleteListerner().attach(
+        'show_notification', getNotification
+    )
+
+def setSession():
+    loadedChallenge = LoadedChallengeGlobDict()
+    playerSession = PlayerGlobDict()
+    puzzleEnvSession = PuzzleEnvGlobalDict()
+    challengeSession = ChallengeGlobDict()
+
+    puzzleEnv.blockPattern = loadedChallenge.challenge
+    puzzleEnv.blockBehavior = loadedChallenge.
+    
+
+def getNotification():
+    showNotification(
+        '15 Puzzle Complete..', duration=5.0, callback=overlayAssessment
+    )
 
 def initPuzzleBoard(pattern):
     puzzle = PuzzleLoader(logic.getCurrentScene())
