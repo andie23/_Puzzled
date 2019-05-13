5e9c09ded419dcadb22eb3be8346dfa392b76a25
Andrew Mfune <andiemfune@gmail.com>
2019-05-12 22:32:21 -0800

    deleted some actions

--- scripts/challenge_menu_actions.py
+++ scripts/challenge_menu_actions.py
@@ -1,24 +1,5 @@
 from global_dictionary import *
-from navigator import navToPuzzle, overlayChallengeView
 from challenge_menu_listerners import OnChallengeListChangeListerner
-
-def __startGame(challenge):
-    ActiveChallengeGlobDictData().create(challenge)
-    navToPuzzle()
-
-def playAction(challenge):
-    if 'instructions' in challenge:
-        overlayDialog()
-        return infoDialog(
-            title =challenge['name'],
-            subtitle =challenge['instructions'],
-            callback = lambda: __startGame(challenge)
-        )
-    __startGame(challenge)
-
-def showChallengeViewer(scene, challenge):
-    ChallengeViewGlobDictData().create(scene, challenge)
-    overlayChallengeView()
 
 def nextChallengeList(paginator, positionNodes):
     paginator.load()
