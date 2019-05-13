9d64b73c8ff2c3a0634db279ee7f9c14a0c5ed2e
Andrew Mfune <andiemfune@gmail.com>
2019-05-12 22:31:11 -0800

    Renamed some listerners to provide proper context.
    Added onrestart and onexit listerners

--- scripts/game_event_listerners.py
+++ scripts/game_event_listerners.py
@@ -35,16 +35,31 @@ class OnGameStartListerner(Listerner):
     def onStart(self):
         self.updateListerners(lambda listerner: listerner())
 
-class OnPuzzleReshuffleListerner(Listerner):
+class OnInvokePuzzleReshuffleListerner(Listerner):
     def __init__(self):
         Listerner.__init__(self, 'ON_PUZZLE_RESHUFFLE')
 
     def onReshuffle(self):
         self.updateListerners(lambda listerner: listerner())
 
-class OnGameQuitListerner(Listerner):
+class OnPuzzleRestartListerner(Listerner):
+    def __init__(self):
+        Listerner.__init__(self, 'ON_PUZZLE_RESTART')
+    
+    def onRestart(self):
+        self.updateListerners(lambda listerner: listerner())
+
+class OnInvokeGameQuitListerner(Listerner):
     def __init__(self):
         Listerner.__init__(self, 'ON_GAME_QUIT')
     
     def onQuit(self):
         self.updateListerners(lambda listerner: listerner())
+
+class OnPuzzleExitListerner(Listerner):
+    def __init__(self):
+        Listerner.__init__(self, 'ON_EXIT')
+    
+    def onExit(self):
+        self.updateListerners(lambda listerner: listerner())
+.
