1487c8faeccf67565bae15d26b85afed989b7a11
Andrew Mfune <andiemfune@gmail.com>
2019-05-12 22:44:47 -0800

    refactored and cleanedout some methods

--- scripts/loader.py
+++ scripts/loader.py
@@ -4,22 +4,7 @@ def add_loading_screen(func):
     from navigator import overlayLoadingScreen
     overlayLoadingScreen()
     def main(*args, **kwargs):
-        if func(*args, **kwargs):
-            logic.globalDict['close_loader'] = True
+        return func(*args, **kwargs)
     return main
 
-def remove_static_loader(objname):
-    def main(func):
-        def submain(*args, **kwargs):
-            if func(*args, **kwargs):
-                scene = logic.getCurrentScene()
-                if objname in scene.objects:
-                    scene.objects[objname].endObject()
-        return submain
-    return main
-
-def runFunc():
-    if 'close_loader' in logic.globalDict:
-        logic.getCurrentScene().end()
-        del logic.globalDict['close_loader']
         
