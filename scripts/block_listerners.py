e6d8847ba0f53b0d83707e2a63d72c0d52ac84fa
Andrew Mfune <andiemfune@gmail.com>
2019-05-15 01:03:04 -0800

    Added a general block movement listerner and refactored the code

--- scripts/block_listerners.py
+++ scripts/block_listerners.py
@@ -1,16 +1,16 @@
 from listerner import Listerner
 from logger import logger
-from global_dictionary import PuzzleSessionGlobalData
 
+log = logger()
 class GeneralBlockListerner(Listerner):
     def __init__(self, channel):
-        listernerContainer = PuzzleSessionGlobalData().listerners
-        Listerner.__init__(self, listernerContainer, channel)
-    
+        from session_listerner_global_data import SessionListernerData
+        super(GeneralBlockListerner, self).__init__(SessionListernerData(channel))
+
 class BlockListerner(GeneralBlockListerner):
     def __init__(self, block, channel):
         channel = '%s_%s' % (block.blockID, channel)
-        GeneralBlockListerner.__init__(self, channel)
+        super(BlockListerner, self).__init__(channel)
         self.block = block
 
 class OnMatchListerner(GeneralBlockListerner):
@@ -18,10 +18,10 @@ class OnMatchListerner(GeneralBlockListerner):
     Non block specific listerner for matched blocks
     '''
     def __init__(self):
-        GeneralBlockListerner.__init__(self, 'ANY_BLOCK_ONMATCH')
-    
+        super(OnMatchListerner, self).__init__('ANY_BLOCK_ONMATCH')
+
     def onMatch(self):
-        self.updateListerners(lambda listerner: listerner(block))
+        self.updateListerners(lambda listerner: listerner())
 
 class OnMisMatchListerner(GeneralBlockListerner):
     '''
@@ -29,58 +29,68 @@ class OnMisMatchListerner(GeneralBlockListerner):
     '''
 
     def __init__(self):
-        GeneralBlockListerner.__init__(self, 'ANY_BLOCK_ONMISMATCH')
+        super(OnMisMatchListerner, self).__init__('ANY_BLOCK_ONMISMATCH')
 
     def onMisMatch(self):
         self.updateListerners(lambda listerner: listerner())
 
 class OnClickBlockListerner(BlockListerner):
     def __init__(self, block):
-        BlockListerner.__init__(self, block, 'CLICK')
+        super(OnClickBlockListerner, self).__init__(block, 'CLICK')
 
     def onClick(self, controller, movableDirection, spaceBlock):
+        log.debug('%s updating onclick listerners %s', self.block.blockID, self.getListerners())
         self.updateListerners(lambda listerner: listerner(
             self.block, controller, movableDirection, spaceBlock)
         )
 
 class OnBlockSlidingListerner(BlockListerner):
     def __init__(self, block):
-        BlockListerner.__init__(self, block, 'ONSLIDING')
+        super(OnBlockSlidingListerner, self).__init__(block, 'ONSLIDING')
     
     def onSliding(self, blockMotion):
         self.updateListerners(lambda listerner: listerner(blockMotion))
 
+class OnBlockMovementListerner(Listerner):
+    def __init__(self):
+        from session_listerner_global_data import SessionListernerData
+        super(OnBlockMovementListerner, self).__init__(SessionListernerData('ON_BLOCK_MOVEMENT'))
+    
+    def onMove(self):
+        self.updateListerners(lambda listerner: listerner())
+
 class OnBlockMovementStartListerner(BlockListerner):
     def __init__(self, block):
-        BlockListerner.__init__(self, block, 'ONMOVE_START')
+        super(OnBlockMovementStartListerner,self).__init__(block, 'ONMOVE_START')
 
     def onStart(self):
         self.updateListerners(lambda listerner: listerner())
 
 class OnBlockMovementStopListerner(BlockListerner):
     def __init__(self, block):
-        BlockListerner.__init__(self, block, 'ONMOVE_STOP')
-    
+        super(OnBlockMovementStopListerner, self).__init__(block, 'ONMOVE_STOP')
+
     def onStop(self, spaceBlock):
         self.updateListerners(lambda listerner: listerner(self.block, spaceBlock))
 
 class OnDetectBlockListerner(BlockListerner):
     def __init__(self, block):
-        BlockListerner.__init__(self, block, 'ONDETECT')
+        super(OnDetectBlockListerner, self).__init__(block, 'ONDETECT')
 
     def onDetect(self, axisname):
+        log.debug('Running %s onBlock detect listerner %s', self.block.blockID, self.getListerners())
         self.updateListerners(lambda listerner: listerner(axisname))
 
 class OnMatchBlockListerner(BlockListerner):
     def __init__(self, block):
-        BlockListerner.__init__(self, block, 'ONMATCH')
+        super(OnMatchBlockListerner, self).__init__(block, 'ON_BLOCK_MATCH')
 
     def onMatch(self):
         self.updateListerners(lambda listerner: listerner())
 
 class OnMisMatchBlockListerner(BlockListerner):
     def __init__(self, block):
-        BlockListerner.__init__(self, block, 'ONMISMATCH')
+        super(OnMisMatchBlockListerner, self).__init__(block, 'ON_BLOCK_MISMATCH')
 
     def onMisMatch(self):
         self.updateListerners(lambda listerner: listerner())
