bd5eb54dd8b8de62b90d13ff1e2d75c12f0df824
Andrew Mfune <andiemfune@gmail.com>
2019-05-15 04:37:59 -0800

    Updated doc string for method that calculates score.
    Minor refactors and fixes have been made

--- scripts/assessment_calculation.py
+++ scripts/assessment_calculation.py
@@ -1,18 +1,20 @@
-from score import Stats, Score
+from pcache import Stats, Scores
 from utils import frmtTime, calcPercDiff
 
 def calculateScore(moves, time, streaks):
     '''
-    A lower number is desirable for a score bacause we want the player
-    to complete a challenge with less moves and time. Higher match streaks 
-    help to reduce the score further to achieve a lower score.
+    The average of time taken to complete the challenge and 
+    moves should be lower. A higher match streak if achieved by the player,
+    is subtracted to average number as a bonus to get a lower number. 
+
+    Note: The lower the number, the better!
     '''
 
     avg = moves + int(time)/ 2
     return avg - streaks
 
-def getPrevScore(playerId, challengeId):
-    score = Score(playerId, challengeId)
+def getBenchmark(playerId, challengeId):
+    score = Scores(playerId, challengeId)
     if score.fetch():
         return score
     
@@ -33,20 +35,20 @@ def updateChallengeStats(playerId, challengeId, time, isWin=False):
             stats.loses +=1
         stats.update()
 
-def updateChallengeBenchmark(playerId, challengeId, moves, time, streaks, points):
-    score = Score(playerId, challengeId)
+def updateChallengeBenchmark(playerId, challengeId, moves, time, streaks, avgScore):
+    score = Scores(playerId, challengeId)
     score.timeCompleted = time
     score.moves = moves
     score.streaks = streaks
-    score.overallScore = points
+    score.overallScore = avgScore
     score.update()
 
-def addChallengeBenchmark(playerId, challengeId, moves, time, streaks, points):
-    score = Score(playerId, challengeId)
+def addChallengeBenchmark(playerId, challengeId, moves, time, streaks, avgScore):
+    score = Scores(playerId, challengeId)
     score.timeCompleted = time
     score.moves = moves
-    score.streaks = matchStreaks
-    score.overallScore = points
+    score.streaks = streaks
+    score.overallScore = avgScore
     score.add()
 
 def assessTime(timeBenchmark, time):
