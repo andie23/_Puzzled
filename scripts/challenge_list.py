7552963d23986affa4c9896e3973b7d86c0ff3b7
Andrew Mfune <andiemfune@gmail.com>
2019-05-15 01:14:49 -0800

    added identifications for each challenge

--- scripts/challenge_list.py
+++ scripts/challenge_list.py
@@ -1,5 +1,6 @@
 CHALLENGE_LIST = [
     {
+        'id' : 1,
         'name': 'A CLASSIC',
         'pattern' : 'CLASSIC',
         'behavior' : 'CLASSIC',
@@ -9,6 +10,7 @@ CHALLENGE_LIST = [
         """
     },
     {
+        'id' : 2,
         'name': 'A SLIGHT TWIST',
         'pattern' : 'PAT_1',
         'behavior' : 'CLASSIC',
@@ -19,6 +21,7 @@ CHALLENGE_LIST = [
         """
     },
     {
+        'id' : 3,
         'name': 'SIDERS',
         'pattern' : 'PAT_2',
         'behavior' : 'CLASSIC',
@@ -28,6 +31,7 @@ CHALLENGE_LIST = [
         """
     },
     {
+        'id' : 4,
         'name': 'BOTTOM-UP APPROACH',
         'pattern' : 'PAT_3',
         'behavior' : 'CLASSIC',
@@ -37,6 +41,7 @@ CHALLENGE_LIST = [
         """
     },
     {
+        'id' : 5,
         'name': 'SIDERS PART 2',
         'pattern' : 'PAT_4',
         'behavior' : 'CLASSIC',
@@ -45,73 +50,101 @@ CHALLENGE_LIST = [
         """
     },
     {
+        'id' : 6,
         'name': 'BOTTOM-UP APPROACH PART 2',
         'pattern' : 'PAT_5',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id': 7,
         'name': 'A SLIGHT TWIST PART 2',
         'pattern' : 'PAT_6',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 8,
         'name': 'LEFT SIDERS',
         'pattern' : 'PAT_7',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 9,
         'name': 'PAIR BY PAIR',
         'pattern' : 'PAT_8',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 10,
         'name': 'LOOP BY LOOP',
         'pattern' : 'PAT_9',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 11,
         'name': 'THE SNAKE',
         'pattern' : 'PAT_10',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 12,
         'name': 'THE SNAKE PART 2',
         'pattern' : 'PAT_11',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 13,
         'name': 'CORNER 2 CORNER',
         'pattern' : 'PAT_12',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 14,
         'name': 'INSEQUENTIAL ROWS',
         'pattern' : 'PAT_13',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 15,
         'name': 'INSEQUENTIAL ROWS PART 2',
         'pattern' : 'PAT_14',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 16,
         'name': 'SWEET SPOT',
         'pattern' : 'PAT_15',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },
     {
+        'id' : 17,
         'name': 'ZIG-ZAG',
         'pattern' : 'PAT_16',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },   
     {
+        'id' : 18,
         'name': 'EVEN AND ODDS',
         'pattern' : 'PAT_17',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     },   
     {
+        'id' : 19,
         'name': 'EVEN AND ODDS PART 2',
         'pattern' : 'PAT_18',
-        'behavior' : 'CLASSIC'
+        'behavior' : 'CLASSIC',
+        'instructions' : ''
     }   
 ]
