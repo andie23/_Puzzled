a67f9c825d929906a620382f79a338fdd2fc0c56
Andrew Mfune <andiemfune@gmail.com>
2019-05-15 21:26:36 -0800

    Restored deleted methods

--- scripts/loader.py
+++ scripts/loader.py
@@ -7,4 +7,17 @@ def add_loading_screen(func):
         return func(*args, **kwargs)
     return main
 
-        
+def remove_static_loader(objname):
+    def main(func):
+        def submain(*args, **kwargs):
+            if func(*args, **kwargs):
+                scene = logic.getCurrentScene()
+                if objname in scene.objects:
+                    scene.objects[objname].endObject()
+        return submain
+    return main
+
+def runFunc():
+    if 'close_loader' in logic.globalDict:
+        logic.getCurrentScene().end()
+        del logic.globalDict['close_loader']
