e8c881d84fc07156e5f80b7d13ba9f377de5f9d3
Andrew Mfune <andiemfune@gmail.com>
2019-05-23 00:28:04 -0800

    added timers for delaying or running animations in a duration.
    Did some refactors and added more docstrings in some modules

--- scripts/playback.py
+++ scripts/playback.py
@@ -1,10 +1,16 @@
 from bge import logic
 from always_instance import AlwaysInstance
+from timer import Timer
 
 class PlayBack():
     '''
-    Animation wrapper for GameObjects to simplify animating
-    them.
+    Animation wrapper for GameObjects.
+
+    Benefits offered beyond the basic animation api provided by blender include:
+        1. Adding callbacks to animations (OnStart, OnFinish, OnFrameChange)
+        2. Delaying animation for a set number of time
+        3. Runnning animation for a duration of time before it's stopped
+        4. Pausing and resuming animations (Coming soon!)
     '''
 
     def __init__(self, obj, animation, fstart, fstop, speed=1.0):
@@ -14,82 +20,111 @@ class PlayBack():
         self.fstop = fstop
         self.speed = speed
     
-    def play(self, onstartAction=None, onfinishAction=None, frameMonitor=None):
+    def play(self, onstartAction=None, onfinishAction=None, 
+             onframeChangeAction=None, duration=0.0, delay=0.0):
         '''
         Play animation only once.
         '''
-        self._start(
-            logic.KX_ACTION_MODE_PLAY, onstartAction, 
-            onfinishAction, frameMonitor
+        self._initAnimation(
+            logic.KX_ACTION_MODE_PLAY, duration, delay, onstartAction, 
+            onfinishAction, onframeChangeAction
         )
 
-    def playLoop(self, onstartAction=None, onfinishAction=None, frameMonitor=None):
+    def playLoop(self, onstartAction=None, onfinishAction=None, 
+                  onframeChangeAction=None, duration=0.0, delay=0.0):
         '''
         Play animation on repeat
         '''
-        
-        self._start(
-            logic.KX_ACTION_MODE_LOOP, onstartAction, 
-            onfinishAction, frameMonitor
+
+        self._initAnimation(
+            logic.KX_ACTION_MODE_LOOP, duration, delay, onstartAction,
+            onfinishAction, onframeChangeAction
         )
        
     def stop(self):
         self.obj.stopAction(0)
 
-    def pause(self):
-        #TODO: pause current playing animation
-        pass
-
-    def resume(self):
-        #TODO: resume paused animation
-        pass
-
     def getAnimId(self):
         return 'anim_%s_%s' % (self.anim, hash(self.obj))
 
-    def _runAnimFrameChecker(self, obj, duration, onstart, onfinish, frameMonitor=None):
+    def _onFrameChange(self, onstartAction=None, onfinishAction=None, onframeChangeAction=None):
         '''
-        Runs modules when the animation starts, is running or when it
-        finishes playing.
-        '''
-        
-        curFrame = obj.getActionFrame(0)
-        if curFrame >= 1.0 and onstart:
-            onstart()
-        
-        if curFrame >= duration and onfinish:
-            onfinish()
-            return True
-        
-        if frameMonitor and obj.isPlayingAction(0):
-            frameMonitor(curFrame)
-        return False
-
-    def _start(self, playMode, onstartAction, onfinishAction, frameMonitor=None):
-        '''
-        Runs animation on a Game object. if actions are specified to run along side 
-        animations, an Always Instance object is created that constantly checks
-        the current playing frame.
+        Runs actions alongside the playing animation
         '''
 
-        # check if actions are set to run at the start of the animation
-        # or at the end
-        if onstartAction or onfinishAction:
-            # Add action in an Always Instance(loop) to constantly
-            # check the current animation frame inorder to excute
-            # starting and ending actions.
-            alwaysInstance = AlwaysInstance(
-                self.getAnimId(), self.obj.scene
-            )
+        def onFrameChange():
+            curFrame = self.obj.getActionFrame(0)
+            if curFrame >= 1.0 and onstartAction:
+                onstartAction()
+            
+            if curFrame >= self.fstop and onfinishAction:
+                onfinishAction()
+                return True
+    
+            if self.obj.isPlayingAction(0) and onframeChangeAction:
+                onframeChangeAction(curFrame)
+            return False
 
-            alwaysInstance.addInstance(
-                lambda: self._runAnimFrameChecker(
-                    self.obj, self.fstop, onstartAction, 
-                    onfinishAction, frameMonitor 
-                )
-            )
+        # Add onFrameChange to an Always Instance to constantly check
+        # the current frame the Game object is playing.
+        alwaysInstance = AlwaysInstance(self.getAnimId(), self.obj.scene)
+        alwaysInstance.addInstance(onFrameChange)
+
+    def _setTimer(self, id, time, action):
+        timer = Timer(id, str(self.obj.scene))
+        if timer.isAlive():
+            timer.destroy()
+
+        if (time >= 1.0):
+            timer.setTimer(time, action)
+            timer.start()
+
+    def _setDuration(self, time):
+        timerId = 'duration_%s' % self.getAnimId()
+        self._setTimer(timerId, time, self.stop)
+
+    def _setDelay(self, time, action):
+        timerId = 'delay_%s' % self.getAnimId()
+        self._setTimer(timerId, time, action)
+
+    def _animateObj(self, mode):
         self.obj.playAction(
-            self.anim, start_frame=self.fstart, 
-            end_frame=self.fstop, speed=self.speed,
-            play_mode=playMode
+            self.anim, 
+            start_frame=self.fstart, 
+            end_frame=self.fstop, 
+            speed=self.speed,
+            play_mode=mode
         )
+
+    def _initAnimation(self, playMode, duration=0.0, delay=0.0, onstartAction=None, 
+                onfinishAction=None, onFrameChange=None):
+        
+        def playAnimation():
+            '''
+            Play animation along with actions if set
+            '''
+
+            if onstartAction or onfinishAction or onFrameChange:
+                self._onFrameChange(onstartAction, onfinishAction, onFrameChange)
+            self._animateObj(playMode)
+    
+        def setDuration():
+            '''
+            play animation until stopped after a period of time
+            '''
+            playAnimation()
+            self._setDuration(duration)
+        
+        if delay >= 1.0 and duration >= 1.0:
+            self._setDelay(delay, setDuration)
+            return
+
+        if delay >= 1.0:
+            self._setDelay(delay, playAnimation)
+            return
+    
+        if duration >= 1.0:
+            setDuration()
+            return
+
+        playAnimation()
