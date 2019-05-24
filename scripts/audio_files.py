8dc8bd980edfd3e85ad45286bb996ada39e96e8b
Andrew Mfune <andiemfune@gmail.com>
2019-05-23 23:56:37 -0800

    Added additional sounds

--- scripts/audio_files.py
+++ scripts/audio_files.py
@@ -5,6 +5,9 @@ def __getSound(soundFile):
     filePath = '//%s/%s' % (SOUND_DIR, soundFile)
     return logic.expandPath(filePath)
 
+ALERT_TONE = __getSound('zapsplat_multimedia_alert_error_002_26393.mp3')
+SINGLE_BEEP = __getSound('zapsplat_multimedia_beep_digital_001_26599.mp3')
+OMNIOUS_BELL_RING = __getSound('skyclad_sound_belldistant_muffled_reverb.mp3')
 SLIDING_BLOCK = __getSound('zapsplat_whoosh.mp3')
 SINGLE_DING = __getSound('zapsplat_desk_bell_single_ring.mp3')
 CLOCK_TICKING = __getSound('soundbites_ticking_clock.mp3')
