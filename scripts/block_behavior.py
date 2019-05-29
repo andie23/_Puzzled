72a537d4b84796db38c3cce0e9524655363403b0
Andrew Mfune <andiemfune@gmail.com>
2019-05-28 23:51:12 -0800

    Added alert behavior.

--- scripts/block_behavior.py
+++ scripts/block_behavior.py
@@ -1,10 +1,9 @@
 from block_effects import *
 from block_listerners import *
+from audio import Audio
+from audio_files import *
 
 def default(block, onMatchListerner, onMisMatchListerner):
-    from audio import Audio
-    from audio_files import SINGLE_DING
-
     vsBlock = block.getVisualBlock()
     onMatchListerner.attach(
         'match_default_behavior', 
@@ -15,3 +14,37 @@ def default(block, onMatchListerner, onMisMatchListerner):
         'mis_match_default_behavior', 
         lambda: animate_default_color(vsBlock)
     )
+
+def redAlert(block, onMatchListerner, onMisMatchListerner):
+    vsblock = block.getVisualBlock()
+    def onMisMatch():
+        def resetTimers(playback):
+            playback.unsetTimers()
+            return True
+        
+        use_red_color(vsblock)
+        Audio(ALERT_TONE).play()
+        annoyingBeepAudio = Audio(SINGLE_BEEP)
+        redFlashPlayback = animate_red_flashes(
+            vsblock, delay=2.0, duration=5.0, speed=0.4,
+            onstartAction=annoyingBeepAudio.play
+        )
+
+        fadeOutPlayBack = animate_to_transparent(
+            vsblock, delay=10.0, 
+            onfinishAction=Audio(OMNIOUS_BELL_RING).play
+        )
+        
+        onMatchListerner.attach('clear_red_flash_timers', 
+            lambda:resetTimers(redFlashPlayback))
+        
+        onMatchListerner.attach('clear_fade_out_timers', 
+            lambda:resetTimers(fadeOutPlayBack))
+            
+    onMatchListerner.attach('match_default_behavior', 
+            lambda: animate_match_color(vsblock))
+    onMatchListerner.attach('play_ring_sound', Audio(SINGLE_DING).play)
+    onMisMatchListerner.attach('mis_match_default_behavior', onMisMatch)
+    onMisMatch()
+
+    
