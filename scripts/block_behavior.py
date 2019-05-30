a12d7348e52450be6dcbc702118c0242ce76b037
Andrew Mfune <andiemfune@gmail.com>
2019-05-29 23:42:35 -0800

    Block listerners are no longer passed as parameters on a behavior.
    All listerners are now imported directly in the module

--- scripts/block_behavior.py
+++ scripts/block_behavior.py
@@ -3,48 +3,30 @@ from block_listerners import *
 from audio import Audio
 from audio_files import *
 
-def default(block, onMatchListerner, onMisMatchListerner):
-    vsBlock = block.getVisualBlock()
-    onMatchListerner.attach(
+def default(block):
+    def onMisMatch(block, wasMatch):
+        if wasMatch:
+           animate_default_color(block.getVisualBlock())
+
+    OnBlockMovementStartListerner().attach(
+        'play_slide_sound', lambda block: Audio(SLIDING_BLOCK).play()
+    )
+
+    OnMatchListerner().attach(
         'match_default_behavior', 
-        lambda: animate_match_color(vsBlock)
+        lambda block: animate_match_color(
+            block.getVisualBlock()
+        )
     )
-    onMatchListerner.attach('play_ring_sound', Audio(SINGLE_DING).play)
-    onMisMatchListerner.attach(
+    OnMatchListerner().attach(
+        'play_ring_sound', lambda block: Audio(SINGLE_DING).play()
+    )
+
+    OnMisMatchListerner().attach(
         'mis_match_default_behavior', 
-        lambda: animate_default_color(vsBlock)
+        lambda block, wasMatch: onMisMatch(block, wasMatch)
     )
 
-def redAlert(block, onMatchListerner, onMisMatchListerner):
-    vsblock = block.getVisualBlock()
-    def onMisMatch():
-        def resetTimers(playback):
-            playback.unsetTimers()
-            return True
-        
-        use_red_color(vsblock)
-        Audio(ALERT_TONE).play()
-        annoyingBeepAudio = Audio(SINGLE_BEEP)
-        redFlashPlayback = animate_red_flashes(
-            vsblock, delay=2.0, duration=5.0, speed=0.4,
-            onstartAction=annoyingBeepAudio.play
-        )
 
-        fadeOutPlayBack = animate_to_transparent(
-            vsblock, delay=10.0, 
-            onfinishAction=Audio(OMNIOUS_BELL_RING).play
-        )
-        
-        onMatchListerner.attach('clear_red_flash_timers', 
-            lambda:resetTimers(redFlashPlayback))
-        
-        onMatchListerner.attach('clear_fade_out_timers', 
-            lambda:resetTimers(fadeOutPlayBack))
-            
-    onMatchListerner.attach('match_default_behavior', 
-            lambda: animate_match_color(vsblock))
-    onMatchListerner.attach('play_ring_sound', Audio(SINGLE_DING).play)
-    onMisMatchListerner.attach('mis_match_default_behavior', onMisMatch)
-    onMisMatch()
 
     
