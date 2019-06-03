35cd781b52d8274c1cee8c71bacca2ee2e029a29
Andrew Mfune <andiemfune@gmail.com>
2019-06-02 23:01:09 -0800

    Block detection now done using python API.
    Using python api to cast rays in 4 directions in spaceblock object.
    
    Controller module for detecting logical blocks is now redundant and has been removed.

--- scripts/sblock.py
+++ scripts/sblock.py
@@ -2,13 +2,31 @@ from block import Block
 from logger import logger
 from bge import logic
  
+DETECTION_POINTS = {
+    'LEFT' : [10.0, 0.0, 0.0],
+    'RIGHT' : [-10.0, 0.0, 0.0],
+    'DOWN' : [0.0, 10.0, 0.0],
+    'UP' : [0.0, -10.0, 0.0]
+}
 log = logger()
+
 class SpaceBlock(Block):
     def __init__(self):
         scene = logic.getCurrentScene()
         super(SpaceBlock, self).__init__(
             scene, scene.objects['space_block']
         )
+
+    def detectLogicalBlocks(self):
+        detectedObjs = {}    
+        for direction, point in DETECTION_POINTS.items():
+            block = self.obj.rayCastTo(
+                point, 1.0, 'logical_block'
+            )
+            if not block:
+                continue
+            detectedObjs[str(block['block_number'])] = direction 
+        return detectedObjs
 
     def setPosition(self, node):
         self.obj.position[0] = node.position[0]
