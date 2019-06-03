18a2a12a84f7d9fca6b46dd84fd0db0d9e3252da
Andrew Mfune <andiemfune@gmail.com>
2019-06-03 00:59:09 -0800

    reduced button click animation speed

--- scripts/button_effects.py
+++ scripts/button_effects.py
@@ -2,6 +2,6 @@ def clickAnimation(button, callback):
     from playback import PlayBack
     
     PlayBack(obj=button.buttonObj, animation='click', 
-        fstart=0.0, fstop=10.0, speed=2.5).play(
+        fstart=0.0, fstop=10.0, speed=1.5).play(
         onfinishAction=callback
     )
