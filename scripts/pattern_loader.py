d5842465660151b77060c214c683bdcf2d900834
Andrew Mfune <andiemfune@gmail.com>
2019-06-14 10:02:18 -0800

    Replaced pattern_visualiser with pattern_loader

---
+++ scripts/pattern_loader.py
@@ -0,0 +1,27 @@
+from bge import logic
+from objproperties import ObjProperties
+from puzzle_loader import PuzzleLoader
+
+class PatternLoader():
+    def __init__(self, scene, pattern):
+        self.scene = scene
+        self.pattern = pattern
+    
+    def load(self):
+        staticBlocks = ObjProperties().getPropObjGroup(
+            'static_block', self.scene
+        )
+        puzzleLoader = PuzzleLoader(self.scene)
+        puzzleLoader.setStaticBlockNumbers(self.pattern, staticBlocks)
+        self._loadVsBlocks(staticBlocks)
+    
+    def _loadVsBlocks(self, staticBlocks):
+        for staticBlock in staticBlocks:
+            vsBlock =  ObjProperties().getObjByPropVal(
+                'visual_block', staticBlock['block_number'], 
+                self.scene.objectsInactive
+            )
+            self.scene.addObject(vsBlock)
+            vsBlock = self.scene.objects[str(vsBlock)]
+            vsBlock.position = staticBlock.position
+            vsBlock.setParent(staticBlock, False, False)
