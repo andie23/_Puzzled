ba85d1d072052d87979fbd0d4e6deb71511b5057
Andrew Mfune <andiemfune@gmail.com>
2019-06-14 10:04:26 -0800

    Added a new dialog to show puzzle pattern and show stats as well as benchmark

--- scripts/challenge_viewer_main.py
+++ scripts/challenge_viewer_main.py
@@ -1,41 +1,50 @@
 from bge import logic
-from pattern_canvas import PatternCanvas
-from navigator import closePatternScreen
+from challenge_viewer_canvas import ChallengeViewerCanvas
 from pcache import Stats, Scores
 from utils import frmtTime
 from player import getPlayerId
-from pattern_visualiser import PatternVisualiser
+from pattern_loader import PatternLoader
+from challenge_global_data import LoadedChallengeGlobalData
+from challenge_viewer_canvas import ChallengeViewerCanvas
+from scene_helper import Scene
 
 def init():
     from challenge_global_data import LoadedChallengeGlobalData
 
+    canvas = addChallengeViewer(
+        Scene('HUD').getscene(), LoadedChallengeGlobalData().getPattern()
+    )
     challengeData = LoadedChallengeGlobalData()
-    playerId = getPlayerId()
-    setScoreCanvas(playerId, challengeData.getName, challengeData.getId())
-    # start visualiser to mark block in a specific pattern
-    PatternVisualiser(logic).visualise(challengeData.getPattern())
+    setScoreCanvas(canvas, getPlayerId(), challengeData.getName, challengeData.getId())
 
-def setScoreCanvas(playerId, challengeName, challengeId):
+    
+def addChallengeViewer(scene, pattern):
+    from objproperties import ObjProperties
+
+    canvas = ChallengeViewerCanvas()
+    canvas.add(scene.objects['information_position_node'])
+    PatternLoader(scene, pattern).load()
+    return canvas
+
+def setScoreCanvas(pcanvas, playerId, challengeName, challengeId):
     from puzzle_main import startPuzzleScene
     from button_widget import Button
     from text_widget import Text
 
     score = Scores(playerId, challengeId)
     stats = Stats(playerId, challengeId)
-    pcanvas = PatternCanvas()
-    pcanvas.load()
-    
+
     playBtn = Button(pcanvas.playBtnObj)
     playBtn.setOnclickAction(lambda: startPuzzleScene())
     
-    returnBtn = Button(pcanvas.backBtnObj)
-    returnBtn.setOnclickAction(closePatternScreen)
+    returnBtn = Button(pcanvas.exitBtnObj)
+    returnBtn.setOnclickAction(pcanvas.remove)
     Text(pcanvas.titleTxtObj, challengeName)
 
     if score.fetch():
-        Text(pcanvas.prevTimeTxtObj, frmtTime(score.timeCompleted))
-        Text(pcanvas.prevMovesTxtObj, score.moves)
-        Text(pcanvas.prevStreakTxtObj, score.streaks)
+        Text(pcanvas.timeTxtObj, frmtTime(score.timeCompleted))
+        Text(pcanvas.movesTxtObj, score.moves)
+        Text(pcanvas.streakCountTxtObj, score.streaks)
 
     if stats.fetch():
         Text(pcanvas.playCountTxtObj, stats.playCount)
