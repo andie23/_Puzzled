b6bad7bd0d49ac5b28fdfe2e925d835fc62b8489
Andrew Mfune <andiemfune@gmail.com>
2019-07-02 14:46:34 -0800

    Readded visual markers to visual blocks when viewing pattern

--- scripts/challenge_viewer_main.py
+++ scripts/challenge_viewer_main.py
@@ -1,5 +1,6 @@
 from bge import logic
 from utils import frmtTime
+from scene_helper import Scene
 
 def init():
     from player import getPlayerId
@@ -13,9 +14,9 @@ def init():
         challengeData.getId()
     )
     menu.open()
+    animateVisualBlocks(challengeData.getId(), menu)
 
 def addChallengeViewer(pattern):
-    from scene_helper import Scene
     from objproperties import ObjProperties
     from pattern_loader import PatternLoader
     from challenge_viewer_canvas import ChallengeViewerCanvas
@@ -52,4 +53,41 @@ def setBenchmarkMenu(menu, playerId, challengeName, challengeId):
         Text(menu.canvas.winsTxtObj, stats.wins)
         Text(menu.canvas.losesTxtObj, stats.loses)
 
+def animateVisualBlocks(challengeName, menu, timerDelay=0.3):
+    from block_effects import animate_match_color
+    from objproperties import ObjProperties
+    from timer import Timer
+    
+    id = 'pattern_visualiser_data'
+    scene = Scene('HUD').getscene()
+
+    if id not in logic.globalDict:
+        logic.globalDict[id] = {
+            'total': len(
+                ObjProperties().getPropObjGroup('visual_block', scene)
+            ), 
+            'index': 0 
+        }
+    else:
+        logic.globalDict[id]['index'] = 0
+
+    def markVisualBlocks():
+        data = logic.globalDict[id]
+        data['index'] += 1
+        if data['index'] <= data['total']:
+            vsBlock = ObjProperties().getObjByPropVal(
+                'visual_block', data['index'], scene.objects
+            )
+            if vsBlock:
+                timer = Timer(id, 'HUD')
+                timer.setTimer(
+                    timerDelay, lambda: animate_match_color(
+                       vsBlock, onfinishAction=markVisualBlocks
+                    )  
+                )
+                timer.start()
+        else:
+            logic.globalDict[id]['index'] = 0
+
+    markVisualBlocks()
 
