d5b1882939846577154835276fa41a46e1b9ae1c
Andrew Mfune <andiemfune@gmail.com>
2019-07-11 09:01:49 -0800

    Added functionality for toggling/swapping two buttons on the UI

--- scripts/button_widget.py
+++ scripts/button_widget.py
@@ -9,22 +9,43 @@ class Button(ButtonGlobalData):
         self.btnIcon = self.buttonObj.children[0]
         self.widget = Widget(buttonObj)
 
+    def toggleBtn(self, anotherBtn, anotherBtnToggleAction, ownToggleAction):
+        '''
+        Swap between buttons continously when clicked on and run their 
+        defined actions
+        '''
+
+        anotherBtn.setOnclickAction(
+            lambda: self._toggle(anotherBtnToggleAction, anotherBtn, self)
+        )
+
+        self.setOnclickAction(
+            lambda: self._toggle(ownToggleAction, self, anotherBtn)
+        )
+
+    def _toggle(self, toggleAction, inactiveBtn, activeBtn):
+        '''
+        Switches active button into camera view and inactive button
+        into off camera view and runs toggle action
+        '''
+
+        from copy import deepcopy
+        onscreenPosition = deepcopy(inactiveBtn.buttonObj.worldPosition)
+        offscreenPosition = deepcopy(activeBtn.buttonObj.worldPosition)
+        
+        activeBtn.buttonObj.worldPosition = onscreenPosition 
+        inactiveBtn.buttonObj.worldPosition = offscreenPosition
+        
+        toggleAction()
+
     def isButtonEnabled(self):
         return self.widget.isParentEnabled() and self.isEnabled()
 
     def hide(self):
-        self.widget.hide()
+        self._setIconVisibility(False)
 
     def show(self):
-        self.widget.show()
-    
-    def _setBtnColor(self, color):
-        if self.widget.isParentEnabled() and self.isEnabled():
-            self.btnIcon.color = color
-    
-    def _runAction(self, action):
-        if self.widget.isParentEnabled() and self.isEnabled():
-            return action()
+        self._setIconVisibility(True)
 
     def applyHoverColor(self):
         self._setBtnColor(self.getOnhoverColor())
@@ -37,3 +58,14 @@ class Button(ButtonGlobalData):
         
     def runOnClickAction(self):
         return self._runAction(self.getOnclickAction())
+
+    def _setIconVisibility(self, bool):
+        self.btnIcon.visible = bool
+
+    def _setBtnColor(self, color):
+        if self.widget.isParentEnabled() and self.isEnabled():
+            self.btnIcon.color = color
+    
+    def _runAction(self, action):
+        if self.widget.isParentEnabled() and self.isEnabled():
+            return action()
