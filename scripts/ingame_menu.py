f69d94cad3955edc1d65f1d2ab780b9e22474d87
Andrew Mfune <andiemfune@gmail.com>
2019-07-11 09:14:06 -0800

    Pause button is now working and toggles with the play/resume button.
    Entire HUD scene is restarted when restart event triggers
    
    refactored runTimer method and renamed it to updateTimerListerners

--- scripts/ingame_menu.py
+++ scripts/ingame_menu.py
@@ -16,6 +16,8 @@ def init(controller):
     from game_event_listerners import OnPuzzleExitListerner
     from game_event_listerners import OnPuzzleCompleteListerner
     from game_event_listerners import OnPuzzleRestartListerner
+    from game_event_listerners import OnGamePauseListerner
+    from game_event_listerners import OnGameResumeListerner
     from block_listerners import OnBlockMovementStartListerner
     from session_global_data import SessionGlobalData
 
@@ -23,12 +25,14 @@ def init(controller):
     menu = Menu(HudCanvas(), BACK_POSITION_NODE)
 
     OnBlockMovementStartListerner().attach('update_moves', lambda b: updateMoveCount())
-    OnPuzzleRestartListerner().attach('restart_clock', Clock(own).reset)
+    OnPuzzleRestartListerner().attach('restart_hud', Scene('HUD').restart)
     OnPuzzleExitListerner().attach('restart_hud', Scene('HUD').restart)
     OnPuzzleCompleteListerner().attach('disable_menu', menu.canvas.disable)
     OnGameStartListerner().attach('display_hud', displayHud)
     OnGameStartListerner().attach('start_clock', Clock(own).start)
     OnGameStartListerner().attach('enable_hud', menu.canvas.enable)
+    OnGamePauseListerner().attach('pause_clock', Clock(own).stop)
+    OnGameResumeListerner().attach('resume_clock', Clock(own).resume)
     OnGameStopListerner().attach('stop_clock', Clock(own).stop)
     HudClockListerner().attach('update_hud_clock', updateHudTimer)
     HudClockListerner().attach('update_session_time', SessionGlobalData().setTime)
@@ -36,35 +40,40 @@ def init(controller):
 
 def displayHud():
     from hud_resources import loadPuzzlePatternViewer
-    from game import reshuffle, pause, quit
+    from game import reshuffle, pause, quit, resume
     from navigator import overlayChallengeViewer
     from button_widget import Button
     from canvas_effects import fadeIn
+    
     canvas = HudCanvas()
-
+    
     pauseBtn = Button(canvas.pauseBtnObj)
+    resumeBtn = Button(canvas.resumeBtnObj)
     homeBtn = Button(canvas.homeBtnObj)
     shuffleBtn = Button(canvas.reshuffleBtnObj)
     patternBtn = Button(canvas.patternBtnObj)
     
     patternBtn.setOnclickAction(loadPuzzlePatternViewer)
-
     shuffleBtn.setOnclickAction(reshuffle)
-    pauseBtn.setOnclickAction(pause)
+    pauseBtn.toggleBtn(resumeBtn, resume, pause)
     homeBtn.setOnclickAction(quit)
 
 def updateHudTimer(curTime):
+    '''
+    Update timer text object with formated time
+    '''
+
     Text(HudCanvas().clockTxtObj, frmtTime(curTime))
 
-def runTimer(controller):
+def updateTimerListerners(controller):
+    '''
+    Get current timer every frame and update listerners
+    '''
     from hud_listerners import HudClockListerner
 
-    own = controller.owner
-    var = ObjProperties(own)
-    isActive = var.getProp('is_timer_active')
-
-    if isActive:
-        HudClockListerner().update(var.getProp('timer'))
+    HudClockListerner().update(
+        Clock(controller.owner).curtime()
+    )
 
 def updateMoveCount():
     from session_global_data import SessionGlobalData
